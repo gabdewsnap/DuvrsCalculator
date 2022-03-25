cb70d485c96f415a06136820a4e48fb8d93218f7
Gabrielle <gabriellemarie@knights.ucf.edu>
2022-03-24 18:37:28 -0700

    Changed so that math is shown as requested by client && an alert is sent if input is empty

--- main.js
+++ main.js
@@ -1,25 +1,6 @@
-document.querySelector('#calculate').addEventListener('click', checkRoute)
-document.querySelector('#calculate').addEventListener('click', addBackground)
+document.querySelector('#calculate').addEventListener('click', checkRoute);
 
-function timeDifference(duvrsAverage, packagesAverage){
-	//Average number of duvrs(mail) and packages for routes 1 through 11
-	let duvrsForToday = document.querySelector('#duvrs').value.trim();
-	let packagesForToday = document.querySelector('#packages').value.trim();
-
-	//So 250 duvrsTime(letters) = 10min and 1 package = 2min
-	let duvrsDifference = duvrsAverage - duvrsForToday;
-	let packagesDifference = packagesAverage - packagesForToday;
-	
-	duvrsDifference = ( duvrsDifference / 250 ) * -10;
-	packagesDifference = packagesDifference * -1
-	console.log(packagesDifference);
-	console.log(duvrsDifference);
-
-	let timeDifference = duvrsDifference + packagesDifference;		
-
-	resultsDisplayed(timeDifference);
-}
-
+//Creates two arrays - One with the different route options and the other with the correspon ding values for the routes.
 function checkRoute(){
 	let route = document.querySelector('select').value;
 
@@ -31,18 +12,47 @@ function checkRoute(){
 	for(let i = 0; i < array.length; i++ ){
 		if(array[i] == route){
 			timeDifference(duvrsAndPackages[i][0], duvrsAndPackages[i][1]);
-
 		}	
 	}
 }
 
+function timeDifference(duvrsAverage, packagesAverage){
+	//Average number of duvrs(mail) and packages for routes 1 through 11
+	let duvrsForToday = document.querySelector('#duvrs').value.trim();
+	let packagesForToday = document.querySelector('#packages').value.trim();
+
+	if(duvrsForToday.length == 0 || packagesForToday.length == 0){
+		alert("Please fill out all information")
+		return;
+	}
+
+	addBackground();
+
+	//So 250 duvrsTime(letters) = 10min and 1 package = 2min
+	let duvrsDifference = duvrsAverage - duvrsForToday;
+	let packagesDifference = packagesAverage - packagesForToday;
+
+	document.getElementById('mathShown').innerHTML= `Duvrs Average: ${duvrsAverage} - ${duvrsForToday} = ${duvrsDifference}`;
+	document.getElementById('mathShown2').innerHTML= `${duvrsDifference} / 250 = ${duvrsDifference/250}`;
+	
+	let duvrsDifferenceCalc = ( duvrsDifference / 250 ) * -10;
+	packagesDifference = packagesDifference * -1
+
+	document.getElementById('mathShown3').innerHTML =`${Math.floor(duvrsDifference/250)} * 10 =  ${Math.floor(duvrsDifferenceCalc)} | ${Math.floor(duvrsDifferenceCalc)} + ${packagesDifference} = ${Math.floor(duvrsDifferenceCalc+packagesDifference)}`;
+
+	let timeDifference = (Math.floor(duvrsDifferenceCalc + packagesDifference));		
+
+	resultsDisplayed(timeDifference);
+}
+
+//Displays the time difference from average on the page
 function resultsDisplayed(timeDifference){
 
 	document.getElementById('routeAverage').innerHTML = '';
 
 	if(timeDifference < 0){
 		timeDifference = timeDifference * -1;
-		document.getElementById('routeAverage').innerHTML =`It will take you ${timeDifference} min less than average`
+		document.getElementById('routeAverage').innerHTML =`It will take you ${timeDifference} min less than average.`
 	}
 	else if (timeDifference > 0){
 		
